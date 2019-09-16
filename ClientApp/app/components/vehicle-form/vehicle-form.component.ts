ec9de72495ca744275c658e0ab28e0f304504a32
homa <homa.pourmohammadi@gmail.com>
2019-09-16 15:26:18 +0430

    Added vehicle-form component and service

---
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import { MakeService } from "../../services/make.service";
+
+@Component({
+  selector: "app-vehicle-form",
+  templateUrl: "./vehicle-form.component.html",
+  styleUrls: ["./vehicle-form.component.css"]
+})
+export class VehicleFormComponent implements OnInit {
+  makes: any;
+
+  constructor(private makeService: MakeService) {}
+
+  ngOnInit() {
+    this.makeService.getMakes().subscribe(makes => (this.makes = makes));
+
+    console.log("this.makes in comp:", this.makes);
+  }
+}
